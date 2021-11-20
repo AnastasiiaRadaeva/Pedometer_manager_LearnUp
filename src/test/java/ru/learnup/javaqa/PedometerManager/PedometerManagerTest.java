7bbdecf7ef566823eaf31ff696cd787ffd23c923
Anastasiia Radaeva <nast.9714@mail.ru>
2021-11-20 21:39:31 +0300

    Added reformat code

--- src/test/java/ru/learnup/javaqa/PedometerManager/PedometerManagerTest.java
+++ src/test/java/ru/learnup/javaqa/PedometerManager/PedometerManagerTest.java
@@ -10,7 +10,7 @@ public class PedometerManagerTest {
     PedometerManager manager = new PedometerManager();
 
     @BeforeEach
-    public void startSettings(){
+    public void startSettings() {
         manager.addSteps(10000);
         manager.addSteps(12000);
         manager.addSteps(7000);
@@ -20,14 +20,14 @@ public class PedometerManagerTest {
 
     @ParameterizedTest
     @CsvSource({"0, -1", "1, 10000", "2, 12000", "5, 20000", "6, -1"})
-    public void checkGetSteps(int day, int expected){
+    public void checkGetSteps(int day, int expected) {
         int actual = manager.getSteps(day);
         Assertions.assertEquals(expected, actual, "CheckGetSteps");
     }
 
     @ParameterizedTest
     @CsvSource({"-1, 6, -1", "0, 6, 0", "200, 6, 200"})
-    public void checkAddSteps(int steps, int day, int expected){
+    public void checkAddSteps(int steps, int day, int expected) {
         manager.addSteps(steps);
         int actual = manager.getSteps(day);
         Assertions.assertEquals(expected, actual, "CheckAddSteps");
@@ -35,8 +35,8 @@ public class PedometerManagerTest {
 
     @ParameterizedTest
     @CsvSource({"0, 0, 5", "1, 21000, 6"})
-    public void checkMaxDay(int flag, int steps, int expected){
-        if (flag == 1){
+    public void checkMaxDay(int flag, int steps, int expected) {
+        if (flag == 1) {
             manager.addSteps(steps);
         }
         int actual = manager.getMaxDay();
@@ -45,7 +45,7 @@ public class PedometerManagerTest {
 
     @ParameterizedTest
     @CsvSource({"0, 2, -1", "6, 8, -1", "1, -1, -1", "1, 3000, 7001", "5, 300, 0", "2, 10000, 0", "1, 10000, 1"})
-    public void checkAdd(int day, int steps, int expected){
+    public void checkAdd(int day, int steps, int expected) {
         int actual = manager.add(day, steps);
         Assertions.assertEquals(expected, actual, "CheckAdd");
     }
